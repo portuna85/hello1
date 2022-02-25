809cf2d57daa654d768e804ad5563cba729f2f85
portuna85 <portuna85@gmail.com>
2022-02-25 22:10:03 +0900

    Update Order

--- src/main/java/com/example/hello/service/OrderServiceImpl.java
+++ src/main/java/com/example/hello/service/OrderServiceImpl.java
@@ -7,14 +7,12 @@ import com.example.hello.domain.Member;
 import com.example.hello.domain.Order;
 import com.example.hello.repositrory.MemberRepository;
 import com.example.hello.repositrory.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-
-
-    private DiscountPolicy discountPolicy;
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
