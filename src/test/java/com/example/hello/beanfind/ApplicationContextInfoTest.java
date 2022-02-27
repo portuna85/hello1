18e3f1fcd6b82a7b879830bfbaf10f420348d4f7
portuna85 <portuna85@gmail.com>
2022-02-27 16:27:08 +0900

    Spring Study Update

--- src/test/java/com/example/hello/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/com/example/hello/beanfind/ApplicationContextInfoTest.java
@@ -1,20 +1,21 @@
 package com.example.hello.beanfind;
 
+import com.example.hello.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContext {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationContext.class);
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("모든 빈 출력")
-    void findAll() {
+    void findAllBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name = " + beanDefinitionName + "object = " + bean);
+            System.out.println("name=" + beanDefinitionName + " object=" + bean);
         }
     }
 
@@ -29,12 +30,11 @@ public class ApplicationContext {
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = " + beanDefinitionName + "object = " + bean);
-
-              */
+            */
 
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
             // Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
-            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_INFRASTRUCTURE) {
                 Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name=" + beanDefinitionName + " object=" + bean);
             }
