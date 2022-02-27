4e6c8d1368b18eeac150fdab3858125496d3c949
portuna85 <portuna85@gmail.com>
2022-02-27 15:07:21 +0900

    Spring Study Update

--- src/test/java/com/example/hello/beanfind/ApplicationContext.java
+++ src/test/java/com/example/hello/beanfind/ApplicationContext.java
@@ -2,6 +2,7 @@ package com.example.hello.beanfind;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContext {
@@ -11,9 +12,34 @@ public class ApplicationContext {
     @DisplayName("모든 빈 출력")
     void findAll() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-
         for (String beanDefinitionName : beanDefinitionNames) {
-
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + "object = " + bean);
         }
     }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            /*
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + "object = " + bean);
+
+              */
+
+            //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            // Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name=" + beanDefinitionName + " object=" + bean);
+            }
+        }
+    }
+
+
 }
