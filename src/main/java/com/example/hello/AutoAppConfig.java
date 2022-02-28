53e0449855f9455cc4996575c3a05a628e5f57ea
portuna85 <portuna85@gmail.com>
2022-02-28 18:19:32 +0900

    Spring Study Update

--- src/main/java/com/example/hello/AutoAppConfig.java
+++ src/main/java/com/example/hello/AutoAppConfig.java
@@ -1,13 +1,21 @@
 package com.example.hello;
 
+import com.example.hello.repositrory.MemberRepository;
+import com.example.hello.repositrory.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+
+import java.lang.reflect.Member;
 
 @Configuration
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
