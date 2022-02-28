8b014b10dc455093c6f395be87358c8b18668900
portuna85 <portuna85@gmail.com>
2022-02-28 18:25:41 +0900

    Spring Study Update

--- src/main/java/com/example/hello/AutoAppConfig.java
+++ src/main/java/com/example/hello/AutoAppConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-import java.lang.reflect.Member;
-
 @Configuration
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
