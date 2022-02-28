76d3b60827bd71c8572c6aa9e47aaabe073e5df3
portuna85 <portuna85@gmail.com>
2022-02-28 15:02:17 +0900

    Spring Study Update

--- src/main/java/com/example/hello/AppConfig.java
+++ src/main/java/com/example/hello/AppConfig.java
@@ -13,6 +13,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberSerivcie -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
