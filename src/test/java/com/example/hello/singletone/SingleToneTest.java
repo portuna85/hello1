860d22a7e2893b5440a5d3ab3184123f8ccb114f
portuna85 <portuna85@gmail.com>
2022-02-28 13:32:06 +0900

    Spring Study Update

--- src/test/java/com/example/hello/singletone/SingleToneTest.java
+++ src/test/java/com/example/hello/singletone/SingleToneTest.java
@@ -5,6 +5,8 @@ import com.example.hello.service.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -41,5 +43,22 @@ public class SingleToneTest {
         System.out.println("singtoneService2 = " + singtoneService2);
 
         assertThat(singtoneService1).isSameAs(singtoneService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        // AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조 값이 다른 것을 확인
+        System.out.println("memberService1 : " + memberService1);
+        System.out.println("memberService2 : " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
 }
