6866881381eca1bc48a24f3633e2477ea26e87f5
portuna85 <portuna85@gmail.com>
2022-03-01 13:31:08 +0900

    Spring Study Update

--- src/test/java/com/example/hello/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/example/hello/lifecycle/BeanLifeCycleTest.java
@@ -1,7 +1,6 @@
 package com.example.hello.lifecycle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +16,7 @@ public class BeanLifeCycleTest {
 
     @Configuration
     static class LifeCycleConfig {
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hellopspring.dev");
--- src/test/java/com/example/hello/lifecycle/NetworkClient.java
+++ src/test/java/com/example/hello/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package com.example.hello.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -28,12 +31,14 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connetc();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
