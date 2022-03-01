f5b498524ad2795fd77f8508b2a56018c9acd329
portuna85 <portuna85@gmail.com>
2022-03-01 14:08:59 +0900

    Spring Study Update

--- src/test/java/com/example/hello/lifecycle/NetworkClient.java
+++ src/test/java/com/example/hello/lifecycle/NetworkClient.java
@@ -1,8 +1,5 @@
 package com.example.hello.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
