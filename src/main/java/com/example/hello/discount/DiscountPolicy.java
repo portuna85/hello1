852b0e33f93cdaad1f2cc33a5f84b528294766d2
portuna85 <portuna85@gmail.com>
2022-03-01 20:48:29 +0900

    Spring Study Update

--- src/main/java/com/example/hello/discount/DiscountPolicy.java
+++ src/main/java/com/example/hello/discount/DiscountPolicy.java
@@ -3,7 +3,7 @@ package com.example.hello.discount;
 import com.example.hello.domain.Member;
 
 public interface DiscountPolicy {
-    /**
+     /*
      * @return 할인 대상 금액
      */
     int discount(Member member, int price);
