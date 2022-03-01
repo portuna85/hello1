3240561395dc95217ac389f21ebc667f51329ce7
portuna85 <portuna85@gmail.com>
2022-03-01 21:17:46 +0900

    Spring Study Update

--- src/main/java/com/example/hello/AppConfig_change.java
+++ src/main/java/com/example/hello/AppConfig_change.java
@@ -1,11 +1,9 @@
 package com.example.hello;
 
-import com.example.hello.discount.FixDiscountPolicy;
 import com.example.hello.repositrory.MemoryMemberRepository;
 import com.example.hello.service.MemberService;
 import com.example.hello.service.MemberServiceImpl;
 import com.example.hello.service.OrderService;
-import com.example.hello.service.OrderServiceImpl;
 
 public class AppConfig_change {
     public MemberService memberSerivce() {
