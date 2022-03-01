da34b90cbdd56c6f192054dca41283bc231ced34
portuna85 <portuna85@gmail.com>
2022-03-01 19:17:53 +0900

    Spring Study Update

---
+++ src/main/java/com/example/hello/controller/LogDemoController.java
@@ -0,0 +1,27 @@
+package com.example.hello.controller;
+
+import com.example.hello.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testID");
+        return "OK";
+    }
+}
---
+++ src/main/java/com/example/hello/controller/LogDemoService.java
@@ -0,0 +1,16 @@
+package com.example.hello.controller;
+
+import com.example.hello.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service ID = " + id);
+    }
+}
