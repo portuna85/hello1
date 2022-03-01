b03d950a2f5322bcabc36a006c360c79d3079b58
portuna85 <portuna85@gmail.com>
2022-03-01 15:31:09 +0900

    Spring Study Update

--- src/test/java/com/example/hello/scope/PrototypeTest.java
+++ src/test/java/com/example/hello/scope/PrototypeTest.java
@@ -14,9 +14,9 @@ public class PrototypeTest {
     @Test
     void prototypeBeanFind() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        System.out.println("prototype Bean1");
+        System.out.println("find prototypeBean1");
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        System.out.println("prototype Bean2");
+        System.out.println("find prototypeBean2");
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         System.out.println("prototypeBean1 = "+prototypeBean1);
         System.out.println("prototypeBean2 = "+prototypeBean2);
