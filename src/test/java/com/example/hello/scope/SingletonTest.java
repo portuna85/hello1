b3b3707996afba4571cb247ddf04f855cd631b93
portuna85 <portuna85@gmail.com>
2022-03-01 15:53:51 +0900

    Spring Study Update

--- src/test/java/com/example/hello/scope/SingletonTest.java
+++ src/test/java/com/example/hello/scope/SingletonTest.java
@@ -1,8 +1,6 @@
 package com.example.hello.scope;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
