6dc1ed35eee125bfe9cee5cbd20f1eaf02baaaa1
portuna85 <portuna85@gmail.com>
2022-03-04 10:19:28 +0900

    Spring Study Update

--- src/main/java/com/example/hello/controller/ReadFileTextAndDIrFile.java
+++ src/main/java/com/example/hello/controller/ReadFileTextAndDIrFile.java
@@ -1,40 +1,43 @@
 package com.example.hello.controller;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadFileTextAndDIrFile {
     public static void main(String[] args) throws IOException {
-        String directory = "d:\\lecture\\";
+        ReadFileTextAndDIrFile readFileTextAndDIrFile = new ReadFileTextAndDIrFile();
+        List<String> file = readFileTextAndDIrFile.findFile();
+        readFileTextAndDIrFile.changeFileName(file);
+    }
 
-        // 파일 내 택스트 읽어 오기
+    public void changeFileName(List<String> fileList) {
+        // 1. file의 파일명 조회
+        // 2. 조회한 파일명을 확장자 분리
+        // 3. file index 에 맞는 lectures index 를 조회
+        // 4. 3번에서 조회된 이름과 2번에서 분리한 확장자를 이용하여 file 이름 변경
+        // 최종: file 에 rename 되어야 함
+        String dir = "d:\\test\\";
+        File file = new File(dir);
+        File[] files = file.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            int fileIdx = file.getName().indexOf(".");
+            String extIdx = file.getName().substring(fileIdx);
+            String fileName = fileList.get(i);
+            file.renameTo(new File(dir + fileName + extIdx));
+        }
+    }
+
+    public List<String> findFile() throws IOException {
         FileReader fileReader = new FileReader("d:\\test.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> list = new ArrayList<>();
 
-        int str;
-        while ((str = fileReader.read()) != -1) {
-            System.out.println((char)str);
+        String str = "";
+        while ((str = bufferedReader.readLine()) != null) {
+            list.add(str);
         }
-        System.out.println("--------------------------------------");
-
-        // 디렉토리 내 파일 리스트 출력  ver 1
-        File dir = new File(directory);
-        String[] list = dir.list();
-
-/*
-                for (int i = 0; i < list.length; i++) {
-            String filename = list[i];
-            System.out.println(filename);
-        }
-        System.out.println("--------------------------------------");
-
-
-        // 디렉토리 내 파일 리스트 출력  ver 2
-        for (String filename : list) {
-            System.out.println(filename);
-        }
-
- */
+        return list;
     }
 }
